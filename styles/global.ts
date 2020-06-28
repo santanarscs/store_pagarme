a8b5f776f06a212c1b61b146caf7e93fa9d99229
santanarscs <raphaelstn@gmail.com>
2020-06-28 18:26:11 -0300

    add style in js with styled-components

---
+++ styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle =  createGlobalStyle`
+     *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: #fff;
+    color: #353535;
+  }
+  body, input, button {
+    font-family: 'Exo 2', sans-serif;
+    font-size: 16px;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+export default GlobalStyle;
