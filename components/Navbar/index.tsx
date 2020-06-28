20ace28666f820353c571c4ac1e23b3dddd26655
santanarscs <raphaelstn@gmail.com>
2020-06-28 18:52:46 -0300

    create navbar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './styles'
+
+const Navbar: React.FC = () => {
+  return (
+    <Container>
+      <h1>Meu e-commerce</h1>
+    </Container>
+  )
+}
+export default Navbar;
---
+++ components/Navbar/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  width: 100%;
+  height: 70px;
+  background: #2196f3;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
