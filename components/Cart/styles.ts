2932188bdfb03f44118243f0783f694c26dac206
Raphael Santana <raphaelstn@gmail.com>
2020-06-29 20:36:46 -0300

    add style in cart

--- components/Cart/styles.ts
+++ components/Cart/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   h2 {
     margin-bottom: 10px;
   }
   ul {
     list-style: none;
     margin: 20px 0;
+    justify-content: flex-start;
     li {
       padding: 5px 0;
       strong {
@@ -29,8 +33,9 @@ export const Container = styled.div`
     text-decoration: none;
     padding: 15px;
     border: none;
-    background: #00e676;
-    color: #fff;
-    border-radius: 5px;
+    background-color: #fcf7e6;
+    border-color: #fcf7e6;
+    color: #bf9d36;
+    text-align: center;
   }
 `;
