50beb430433884f5d47c7716c807355ba18d535e
Raphael Santana <raphaelstn@gmail.com>
2020-07-01 19:42:53 -0300

    add unform in profile page

---
+++ components/Form/Input/index.tsx
@@ -0,0 +1,25 @@
+import React, { useRef, useEffect } from 'react';
+import { useField } from '@unform/core';
+export default function Input({ name, label, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+  return (
+    <>
+      <label htmlFor={fieldName}>{label}</label>
+      <input
+        id={fieldName}
+        ref={inputRef}
+        defaultValue={defaultValue}
+        {...rest}
+      />
+      {error && <span className="error">{error}</span>}
+    </>
+  );
+}
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,70 +1,78 @@
-import React from 'react'
+import React, {useEffect, useRef, useCallback} from 'react'
+import {Form } from '@unform/web'
+import { SubmitHandler, FormHandles, Scope } from '@unform/core'
 import { Container, Row, InputGroup } from '../styles/profile'
+import Input from '../components/Form/Input'
 import { useAuth } from '../hooks/auth'
+
+
+interface FormData {
+  name: string,
+  email: string,
+  
+}
+
 const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit: SubmitHandler<FormData> = useCallback(async (data) => {
+    console.log(data)
+  }, [])
   const { user } = useAuth()
   return (
     <Container>
       <h1>Olá, {user?.name}</h1>
-      <form>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={user}>
         <fieldset>
           <h3>Dados Gerais</h3>
           <Row>
             <InputGroup style={{marginRight: 20}}>
-              <label htmlFor="name">Nome Completo</label>
-              <input type="text" name="name" placeholder="Seu nome completo"/>
+              <Input label="Nome Completo" type="text" name="name" placeholder="Seu nome completo"/>
             </InputGroup>
             <InputGroup>
-              <label htmlFor="phone">Telefone</label>
-              <input type="text" name="phone" placeholder="Seu telefone"/>
+              <Input label="Telefone" type="text" name="phone" placeholder="Seu telefone"/>
             </InputGroup>
           </Row>
           <Row>
             <InputGroup style={{marginRight: 20}}>
-              <label htmlFor="email">E-mail</label>
-              <input type="email" name="email" placeholder="Seu e-mail"/>
+               <Input label="E-mail" type="email" name="email" placeholder="Seu e-mail"/>
             </InputGroup>
             <InputGroup>
-              <label htmlFor="cpf">CPF</label>
-              <input type="text" name="cpf" placeholder="Seu CPF"/>
+              <Input label="CPF" type="text" name="cpf" placeholder="Seu CPF"/>
             </InputGroup>
           </Row>
         </fieldset>
         <fieldset>
           <h3>Endereço</h3>
-          <Row>
-            <InputGroup style={{marginRight: 20}}>
-              <label htmlFor="zipcode">CEP</label>
-              <input type="text" name="zipcode" placeholder="Seu CEP"/>
-            </InputGroup>
-            <InputGroup style={{marginRight: 20}}>
-              <label htmlFor="street">Rua</label>
-              <input type="text" name="street" placeholder="Rua ex."/>
-            </InputGroup>
-            <InputGroup>
-              <label htmlFor="street_number">Número</label>
-              <input type="text" name="street_number" placeholder="Número"/>
-            </InputGroup>
-          </Row>
-          <Row>
-            <InputGroup>
-              <label htmlFor="neighborhood">Bairro</label>
-              <input type="text" name="neighborhood" placeholder="Bairro"/>
-            </InputGroup>
-          </Row>
-          <Row>
-            <InputGroup style={{marginRight: 20}}>
-              <label htmlFor="city">Cidade</label>
-              <input type="text" name="city" placeholder="Cidade"/>
-            </InputGroup>
-            <InputGroup>
-              <label htmlFor="state">Estado</label>
-              <input type="text" name="state" placeholder="Estado"/>
-            </InputGroup>
-          </Row>
+          <Scope path="address">
+            <Row>
+              <InputGroup style={{marginRight: 20}}>
+                <Input label="CEP" type="text" name="zipcode" placeholder="Seu CEP"/>
+              </InputGroup>
+              <InputGroup style={{marginRight: 20}}>
+                <Input label="Rua" type="text" name="street" placeholder="Rua ex."/>
+              </InputGroup>
+              <InputGroup>
+                <Input label="Número" type="text" name="street_number" placeholder="Número"/>
+              </InputGroup>
+            </Row>
+            <Row>
+              <InputGroup>
+                <Input label="bairro" type="text" name="neighborhood" placeholder="Bairro"/>
+              </InputGroup>
+            </Row>
+            <Row>
+              <InputGroup style={{marginRight: 20}}>
+                <Input label="Cidade" type="text" name="city" placeholder="Cidade"/>
+              </InputGroup>
+              <InputGroup>
+                <Input label="Estado" type="text" name="state" placeholder="Estado"/>
+              </InputGroup>
+            </Row>
+          </Scope>
         </fieldset>
         <button type="submit">Atualizar</button>
-      </form>
+      </Form>
     </Container>
   )
 }
