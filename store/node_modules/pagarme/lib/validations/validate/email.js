d84b560d217271825d4c900f5a1d8e988c350e4a
Raphael Santana <raphaelstn@gmail.com>
2020-07-02 15:26:58 -0300

    remove folder

--- store/node_modules/pagarme/lib/validations/validate/email.js
+++
@@ -1,6 +0,0 @@
-import { test } from 'ramda'
-
-// eslint-disable-next-line no-useless-escape
-const validate = test(/^([a-zA-Z0-9_\.\-\+])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)
-
-export default validate
